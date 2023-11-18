aa85121376998b63eaee9dcd223dbbe155b6c8b9
Srinivasan R <124020324+Srinivasan-Ravi@users.noreply.github.com>
2023-11-18 22:32:19 +0530

    Add files via upload

---
+++ ImageCaptioning/system.js
@@ -0,0 +1,18 @@
+document.getElementById('imageInput').addEventListener('change', function (e) {
+    var file = e.target.files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function (readerEvent) {
+            document.getElementById('selectedImage').src = readerEvent.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+function captionImage() {
+    // In a real application, this is where you would make a server request
+    // to get the image caption using a machine learning model.
+    // For simplicity, let's just display a static caption here.
+    var resultElement = document.getElementById('captionResult');
+    resultElement.textContent = 'A beautiful image';
+}
